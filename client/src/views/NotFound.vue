23cafe0347bc5e903eeea8a40df7b7405db4eb1c
canberk-yilmaz <cyilmaz089@gmail.com>
2021-12-11 23:42:09 +0300

    feat: 404 page create

---
+++ client/src/views/NotFound.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-container fluid>
+    <v-layout row wrap>
+      <v-flex xs12 class="text-center" mt-5>
+        <h1>Error - page not found</h1>
+        <div class="mt-4">
+          <router-link to="/">Go To HomePage</router-link>
+        </div>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+<script>
+export default {}
+</script>
