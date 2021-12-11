d8539b3b4c1e596368df3dfe01b1f3b43976c433
canberk-yilmaz <cyilmaz089@gmail.com>
2021-12-11 23:46:06 +0300

    feat: project routes add with lazy loading

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,30 +1,68 @@
-import Vue from "vue";
-import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
-
-Vue.use(VueRouter);
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Home from '../views/Home.vue'
+Vue.use(VueRouter)
 
 const routes = [
   {
-    path: "/",
-    name: "Home",
+    path: '/',
+    name: 'Home',
     component: Home,
   },
   {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
+    path: '/dashboard',
+    name: 'Dashboard',
     component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue"),
+      import(/* webpackChunkName: "dashboard" */ '../views/Dashboard.vue'),
   },
-];
+  {
+    path: '/signin',
+    name: 'SignIn',
+    component: () =>
+      import(/* webpackChunkName: "signin" */ '../views/SignIn.vue'),
+  },
+  {
+    path: '/signup',
+    name: 'SignUp',
+    component: () =>
+      import(/* webpackChunkName: "signup" */ '../views/SignUp.vue'),
+  },
+  {
+    path: '/settings',
+    name: 'UserSettings',
+    component: () =>
+      import(/* webpackChunkName: "signup" */ '../views/UserSettings.vue'),
+  },
+  {
+    path: '/404',
+    alias: '*',
+    name: 'notFound',
+    component: () =>
+      import(/* webpackChunkName: "NotFound" */ '../views/NotFound.vue'),
+  },
+]
 
 const router = new VueRouter({
-  mode: "history",
+  mode: 'history',
   base: process.env.BASE_URL,
   routes,
-});
+})
 
-export default router;
+// router.beforeEach((to, from, next) => {
+//   if (to.matched.some((record) => record.meta.requiresAuth)) {
+//     // this route requires auth, check if logged in
+//     // if not, redirect to login page.
+//     if (!auth.loggedIn()) {
+//       next({
+//         path: '/login',
+//         query: { redirect: to.fullPath },
+//       })
+//     } else {
+//       next()
+//     }
+//   } else {
+//     next() // make sure to always call next()!
+//   }
+// })
+
+export default router
