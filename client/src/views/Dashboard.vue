87c0bbeeed7574b9b8fcd5ef26d36faeb02974ef
canberk-yilmaz <cyilmaz089@gmail.com>
2021-12-11 23:45:27 +0300

    feat: dashboard create with sample data

---
+++ client/src/views/Dashboard.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <TheDashboard />
+  </div>
+</template>
+
+<script>
+import TheDashboard from '../components/TheDashboard'
+
+export default {
+  name: 'Dashboard',
+
+  components: {
+    TheDashboard,
+  },
+}
+</script>
