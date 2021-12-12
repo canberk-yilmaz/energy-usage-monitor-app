5173690fed627f33356f97cee0d8cb7c4cfc144c
canberk-yilmaz <cyilmaz089@gmail.com>
2021-12-13 00:10:53 +0300

    feat: mongo server create

--- server/index.js
+++ server/index.js
@@ -1,1 +1,44 @@
-console.log('test')
+const express = require('express')
+const mongoose = require('mongoose')
+const cors = require('cors')
+const user = require('./router/user')
+const log = require('./middleware/log')
+const logs = require('./router/logs')
+const bodyParser = require('body-parser')
+
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
+const app = express()
+app.use(cors())
+app.use(express.urlencoded({ extended: true }))
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+mongoose.connect(
+  process.env.mongoURI,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+  (err) => {
+    if (err) {
+      console.log(err)
+    } else {
+      console.log('Connected to mongodb...')
+    }
+  }
+)
+
+app.listen(process.env.PORT || 3000, (err) => {
+  if (err) {
+    console.log(err)
+  } else {
+    console.log(process.env.PORT + 'listening...')
+  }
+})
+
+app.use('/api/user', log, user)
+app.use('/api/logs', logs)
