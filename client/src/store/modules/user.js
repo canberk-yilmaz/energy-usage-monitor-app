252fb55963dca9eb69927002e772286ee74f089f
canberk-yilmaz <cyilmaz089@gmail.com>
2021-12-13 00:07:00 +0300

    build: vuex user module create

---
+++ client/src/store/modules/user.js
@@ -0,0 +1,14 @@
+import axios from 'axios'
+import jwt from 'jsonwebtoken'
+
+export default {
+  namespaced: true,
+  state: {
+    user: null,
+    authStatus: null,
+    authError: false,
+  },
+  mutations: {},
+  actions: {},
+  getters: {},
+}
