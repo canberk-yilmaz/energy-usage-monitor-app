373a7eba0b85d4cf98209122c15f8fbec92a351d
canberk-yilmaz <cyilmaz089@gmail.com>
2021-12-13 01:52:24 +0300

    feat: username update request handle

--- server/router/user.js
+++ server/router/user.js
@@ -130,4 +130,35 @@ router.post('/settings/', async (req, res) => {
   }
 })
 
+router.put(
+  '/settings/',
+  [
+    check('username', 'Username is not valid')
+      .not()
+      .isEmpty()
+      .not()
+      .matches(/\d/)
+      .isLength({ min: 8 }),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req)
+    if (!errors.isEmpty()) {
+      return res.status(406).json({
+        errors: errors.array(),
+      })
+    }
+
+    console.log(req.body)
+    try {
+      const doc = await User.findOneAndUpdate(
+        { id: req.body.id },
+        { username: req.body.username }
+      )
+      res.status(201).json({ doc })
+    } catch (e) {
+      res.send('User Settings Error')
+    }
+  }
+)
+
 module.exports = router
