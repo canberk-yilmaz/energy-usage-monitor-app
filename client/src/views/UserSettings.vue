209b64cb4ed6c3414993f877f6a58284275052e4
canberk-yilmaz <cyilmaz089@gmail.com>
2021-12-13 00:09:43 +0300

    feat: sample post request create to display user

--- client/src/views/UserSettings.vue
+++ client/src/views/UserSettings.vue
@@ -1,17 +1,34 @@
 <template>
-  <v-container fluid>
+  <v-container fluid center>
     <v-layout row wrap>
-      <v-flex xs12 class="text-center" mt-5>
+      <v-flex xs12 class="text-center" mt-5 mb-5>
         <h1>User Settings</h1>
       </v-flex>
-      <v-flex xs12 class="text-center" mt-3>
-        <p>This is a user's home page</p>
-      </v-flex>
     </v-layout>
+    <v-flex xs12 class="text-center" mt-5>
+      <p>Username: {{ user.username }}</p>
+      <p>E-mail: {{ user.email }}</p>
+    </v-flex>
   </v-container>
 </template>
 <script>
+import axios from 'axios'
+
 export default {
   name: 'UserSettings',
+  data: () => ({
+    user: '',
+  }),
+  methods: {
+    async getUserInfo() {
+      const res = await axios.post('/api/user/settings/', {
+        id: '61b5d5cd04ceb5cd348c6d9e',
+      })
+      this.user = res.data.user
+    },
+  },
+  created() {
+    this.getUserInfo()
+  },
 }
 </script>
