a77bc9071cff6ac3d658cae855a55c1b071a69c9
canberk-yilmaz <cyilmaz089@gmail.com>
2021-12-15 19:51:16 +0300

    build: vuelidate add

--- client/src/main.js
+++ client/src/main.js
@@ -4,6 +4,9 @@ import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify'
 import i18n from './i18n'
+import Vuelidate from 'vuelidate'
+
+Vue.use(Vuelidate)
 
 Vue.config.productionTip = false
 
