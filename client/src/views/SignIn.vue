c889284d0058271ff6ae42c4241f25bc800c216d
canberk-yilmaz <cyilmaz089@gmail.com>
2021-12-15 19:58:10 +0300

    feat: auth func add

--- client/src/views/SignIn.vue
+++ client/src/views/SignIn.vue
@@ -1,34 +1,42 @@
 <template>
-  <v-container fluid>
+  <v-container fluid center>
     <v-layout row wrap>
       <v-flex xs12 class="text-center" mt-5>
         <h1>Sign In</h1>
       </v-flex>
       <v-flex xs12 sm6 offset-sm3 mt-3>
-        <form>
-          <v-layout column>
-            <v-flex>
-              <v-text-field
-                name="email"
-                label="Email"
-                id="email_signin"
-                type="email"
-                required
-              ></v-text-field>
-            </v-flex>
-            <v-flex>
-              <v-text-field
-                name="password"
-                label="Password"
-                id="password_signin"
-                type="password"
-                required
-              ></v-text-field>
-            </v-flex>
-            <v-flex class="text-xs-center" mt-5>
-              <v-btn color="primary" type="submit">Sign In</v-btn>
-            </v-flex>
-          </v-layout>
+        <form @submit.prevent="getFormValues">
+          <v-text-field
+            v-model="email"
+            :error-messages="emailErrors"
+            name="email"
+            label="Email"
+            id="email"
+            type="email"
+            required
+            @input="$v.email.$touch()"
+            @blur="$v.email.$touch()"
+          ></v-text-field>
+
+          <v-text-field
+            v-model="password"
+            :error-messages="passwordErrors"
+            name="password"
+            label="Password"
+            id="password"
+            type="password"
+            required
+            @input="$v.password.$touch()"
+            @blur="$v.password.$touch()"
+          ></v-text-field>
+          <v-checkbox v-model="rememberme" :label="'Remember Me'"></v-checkbox>
+
+          <v-flex class="text-center" mt-5>
+            <v-btn color="primary" @click="getFormValues">Sign In</v-btn>
+          </v-flex>
+          <v-flex v-if="error" class="text-center" mt-5>
+            <p class="red--text">Please check your email and password</p>
+          </v-flex>
         </form>
       </v-flex>
     </v-layout>
@@ -36,7 +44,89 @@
 </template>
 
 <script>
+import axios from 'axios'
+const jwt = require('jsonwebtoken')
+import { required, email } from 'vuelidate/lib/validators'
+import { mapGetters } from 'vuex'
+
 export default {
+  validations: {
+    email: { required, email },
+    password: {
+      required,
+    },
+  },
+
   name: 'SignIn',
+  data() {
+    return {
+      email: '',
+      password: '',
+      error: false,
+      rememberme: false,
+    }
+  },
+  methods: {
+    setUserData() {
+      this.email = localStorage.getItem('userEmail') || ''
+      this.password = localStorage.getItem('userPassword') || ''
+    },
+    async userSignIn(data) {
+      try {
+        const res = await axios.post(
+          'http://localhost:3000/api/user/signin',
+          data
+        )
+        const token = res.data.token
+        if (jwt.verify(token, process.env.VUE_APP_SECRET_KEY)) {
+          res.data.remember = this.rememberme
+          if (this.rememberme) {
+            localStorage.setItem('userEmail', this.email)
+            localStorage.setItem('userPassword', this.password)
+          }
+          this.$store.dispatch('setUser', res.data)
+          setTimeout(() => {
+            this.$router.push('/dashboard')
+          }, 1000)
+        }
+        this.error = false
+      } catch (error) {
+        this.error = error.response.data
+      }
+    },
+    getFormValues() {
+      if (this.$v.$invalid) {
+        this.$v.$touch()
+      } else {
+        this.userData = {
+          email: this.email,
+          password: this.password,
+        }
+        this.userSignIn(this.userData)
+      }
+    },
+  },
+  computed: {
+    emailErrors() {
+      const errors = []
+      if (!this.$v.email.$dirty) return errors
+      !this.$v.email.email && errors.push('Must be valid e-mail')
+      !this.$v.email.required && errors.push('E-mail is required')
+      return errors
+    },
+    passwordErrors() {
+      const errors = []
+      if (!this.$v.password.$dirty) return errors
+      !this.$v.password.required && errors.push('Please check your password')
+      return errors
+    },
+    ...mapGetters(['getUser']),
+  },
+  created() {
+    this.setUserData()
+    if (this.getUser) {
+      console.log('you are already logged in')
+    }
+  },
 }
 </script>
--- client/src/views/SignUp.vue
+++ client/src/views/SignUp.vue
@@ -6,49 +6,55 @@
       </v-flex>
       <v-flex xs12 sm6 offset-sm3 mt-3>
         <form @submit.prevent="getFormValues">
-          <v-layout column>
-            <v-flex>
-              <v-text-field
-                v-model="email"
-                name="email"
-                label="Email"
-                id="email"
-                type="email"
-                required
-              ></v-text-field>
-            </v-flex>
-            <v-flex>
-              <v-text-field
-                v-model="password"
-                name="password"
-                label="Password"
-                id="password"
-                type="password"
-                required
-              ></v-text-field>
-            </v-flex>
-            <v-flex>
-              <v-text-field
-                v-model="username"
-                name="username"
-                label="Username"
-                id="username"
-                required
-              ></v-text-field>
-            </v-flex>
-            <v-row align="center">
-              <v-col class="d-flex" cols="12">
-                <v-select
-                  v-model="userStatus"
-                  :items="items"
-                  label="User Status"
-                ></v-select>
-              </v-col>
-            </v-row>
-            <v-flex class="text-center" mt-5>
-              <v-btn color="primary" type="submit">Sign Up</v-btn>
-            </v-flex>
-          </v-layout>
+          <v-text-field
+            v-model="email"
+            :error-messages="emailErrors"
+            name="email"
+            label="Email"
+            id="email"
+            type="email"
+            required
+            @input="$v.email.$touch()"
+            @blur="$v.email.$touch()"
+          ></v-text-field>
+
+          <v-text-field
+            v-model="password"
+            :error-messages="passwordErrors"
+            name="password"
+            label="Password"
+            id="password"
+            required
+            @input="$v.password.$touch()"
+            @blur="$v.password.$touch()"
+          ></v-text-field>
+
+          <v-text-field
+            v-model="username"
+            :error-messages="usernameErrors"
+            name="username"
+            label="Username"
+            id="username"
+            required
+            @input="$v.username.$touch()"
+            @blur="$v.username.$touch()"
+          ></v-text-field>
+
+          <v-row align="center">
+            <v-col class="d-flex" cols="12">
+              <v-select
+                v-model="userStatus"
+                :error-messages="userStatusErrors"
+                :items="items"
+                label="User Status"
+                @change="$v.userStatus.$touch()"
+                @blur="$v.userStatus.$touch()"
+              ></v-select>
+            </v-col>
+          </v-row>
+          <v-flex class="text-center" mt-5>
+            <v-btn color="primary" @click="getFormValues">Sign Up</v-btn>
+          </v-flex>
         </form>
       </v-flex>
     </v-layout>
@@ -56,9 +62,36 @@
 </template>
 
 <script>
+import axios from 'axios'
+import {
+  required,
+  minLength,
+  email,
+  helpers,
+  alpha,
+} from 'vuelidate/lib/validators'
+
+const hasLowerCase = helpers.regex('hasLowerCase', /[a-z]/)
+
+const hasUpperCase = helpers.regex('hasUpperCase', /[A-Z]/)
+
+const hasNumber = helpers.regex('hasUpperCase', /[0-9]/)
+
 export default {
+  validations: {
+    email: { required, email },
+    password: {
+      required,
+      hasLowerCase,
+      hasUpperCase,
+      minLength: minLength(8),
+      hasNumber,
+    },
+    username: { required, minLength: minLength(8), alpha },
+    userStatus: { required },
+  },
   data: () => ({
-    items: ['Admin', 'Editor', 'User'],
+    items: ['admin', 'editor', 'user'],
     email: '',
     password: '',
     username: '',
@@ -66,19 +99,82 @@ export default {
     userData: {},
   }),
   methods: {
-    getFormValues() {
-      this.userData = {
-        email: this.email,
-        password: this.password,
-        username: this.username,
-        userStatus: this.userStatus,
+    async userSignUp(data) {
+      try {
+        const res = await axios.post(
+          'http://localhost:8080/api/user/signup',
+          data
+        )
+        console.log(res)
+      } catch (error) {
+        console.log(error + 'Please control your credentials')
       }
-      console.log(this.userData)
-      this.email = ''
-      this.password = ''
-      this.username = ''
-      this.userStatus = ''
     },
+    getFormValues() {
+      if (this.$v.$invalid) {
+        this.$v.$touch()
+      } else {
+        this.userData = {
+          username: this.username,
+          email: this.email,
+          password: this.password,
+          role: this.userStatus,
+        }
+        this.userSignUp(this.userData)
+        setTimeout(() => {
+          this.$router.push('/user/signin')
+        }, 1000)
+      }
+    },
+  },
+  computed: {
+    emailErrors() {
+      const errors = []
+      if (!this.$v.email.$dirty) return errors
+      !this.$v.email.email && errors.push('Must be valid e-mail')
+      !this.$v.email.required && errors.push('E-mail is required')
+      return errors
+    },
+    passwordErrors() {
+      const errors = []
+      if (!this.$v.password.$dirty) return errors
+      !this.$v.password.required && errors.push('Password is required')
+      !this.$v.password.hasLowerCase &&
+        errors.push('Password must contain lowercase letter.')
+      !this.$v.password.hasUpperCase &&
+        errors.push('Password must contain uppercase letter.')
+      !this.$v.password.minLength &&
+        errors.push('Password should be minimum 8 characters')
+      !this.$v.password.hasNumber && errors.push('Password must contain number')
+      return errors
+    },
+    usernameErrors() {
+      const errors = []
+      if (!this.$v.username.$dirty) return errors
+      !this.$v.username.required && errors.push('Username is required')
+      !this.$v.username.alpha &&
+        errors.push('Username should not contain numbers.')
+      !this.$v.username.minLength &&
+        errors.push('Username should be minimum 8 characters')
+      return errors
+    },
+    userStatusErrors() {
+      const errors = []
+      if (!this.$v.userStatus.$dirty) return errors
+      !this.$v.userStatus.required && errors.push('User Status is required')
+      return errors
+    },
+  },
+  created() {
+    this.email = ''
+    this.password = ''
+    this.username = ''
+    this.userStatus = ''
   },
 }
 </script>
+<style scoped>
+.error {
+  color: white;
+}
+</style>
