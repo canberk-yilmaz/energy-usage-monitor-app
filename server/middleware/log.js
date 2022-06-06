c1eb76c4a0aafb9388697e14cbdff3793b97f03e
canberk-yilmaz <cyilmaz089@gmail.com>
2022-06-06 16:43:21 +0300

    cors error handle

--- server/middleware/log.js
+++ server/middleware/log.js
@@ -1,5 +1,25 @@
 const Log = require('../models/log')
 module.exports = async (req, res, next) => {
+  //set header first to allow request or origin domain (value can be different)
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  )
+  res.setHeader('Access-Control-Allow-Credentials', true)
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET, POST, PUT, PATCH, OPTIONS, DELETE'
+  )
+
+  //---- other code
+
+  //Preflight CORS handler
+  if (req.method === 'OPTIONS') {
+    return res.status(200).json({
+      body: 'OK',
+    })
+  }
   try {
     let requestBody = {
       id: req.body.id,
--- server/router/logs.js
+++ server/router/logs.js
@@ -1,13 +1,33 @@
-const router = require('express').Router();
+const router = require('express').Router()
 const Log = require('../models/log')
 
 router.get('/all', async (req, res) => {
-    try {
-        const logs = await Log.find()
-        res.status(201).json({logs, LogsLength: logs.length})
-    } catch (e) {
-        res.send('me error')
-    }
+  //set header first to allow request or origin domain (value can be different)
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  )
+  res.setHeader('Access-Control-Allow-Credentials', true)
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET, POST, PUT, PATCH, OPTIONS, DELETE'
+  )
+
+  //---- other code
+
+  //Preflight CORS handler
+  if (req.method === 'OPTIONS') {
+    return res.status(200).json({
+      body: 'OK',
+    })
+  }
+  try {
+    const logs = await Log.find()
+    res.status(201).json({ logs, LogsLength: logs.length })
+  } catch (e) {
+    res.send('me error')
+  }
 })
 
 module.exports = router
